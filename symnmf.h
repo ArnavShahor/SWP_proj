04b4acb233eeb8732da5b902bbe503fb55719a45
Gal Barak <g_barak@apple.com>
2025-09-22 14:41:21 +0300

    clean header file

--- symnmf.h
+++ symnmf.h
@@ -14,45 +14,21 @@ typedef double **matrix;
 #define ERROR 1
 #define GENERIC_ERROR_MSG "An Error Has Occurred"
 
-/* TODO should we filter header file and keep only module-related functions? */
-
 /* Matrix memory management */
 matrix alloc_matrix(int rows, int cols);
 void free_matrix(matrix mat);
 
-/* File I/O functions */
-int count_dimensions(const char *filename);
-int count_lines(const char *filename);
-int read_matrix_data(FILE *file, matrix result, int n, int d);
+/* File parsing (needed by main program) */
 matrix parse_file(const char *filename, int *n, int *d);
 
 /* Main program helpers */
 matrix execute_goal(const char *goal, matrix datapoints, int n, int d);
-
-/* Utility functions */
-double euclidean_distance_squared(double *p1, double *p2, int d);
-void copy_matrix(matrix src, matrix dst, int rows, int cols);
 void print_matrix(matrix mat, int rows, int cols);
 
-/* Matrix operations */
-matrix matrix_multiply(matrix A, matrix B, int n1, int m1, int m2);
-matrix transpose_matrix(matrix A, int rows, int cols);
-double F_norm_squared(matrix A, matrix B, int rows, int cols);
-
-/* Degree calculations */
-double *calculate_degrees_array(matrix A, int n);
-matrix create_diagonal_matrix(double *degrees, int n);
-void normalize_degrees(double *degrees, int n);
-matrix create_normalized_matrix(matrix A, double *degrees, int n);
-
-/* Core SymNMF algorithms */
+/* Core SymNMF algorithms - Public API */
 matrix sym_c(matrix datapoints, int n, int d);
 matrix ddg_c(matrix datapoints, int n, int d);
 matrix norm_c(matrix datapoints, int n, int d);
-
-/* SymNMF iteration functions */
-matrix compute_H_denominator(matrix H, int n, int k);
-matrix update_H_iteration(matrix H, matrix W, int n, int k);
 matrix symnmf_c(matrix H, matrix W, int n, int k);
 
 #endif /* SYMNMF_H */
